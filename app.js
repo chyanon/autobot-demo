eb68364a33890b9454a0d352ffaca512e1e73d3a
chyanon <36694846+chyanon@users.noreply.github.com>
2018-06-12 16:25:57 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
+    res.sendStatus(200)
+})
 app.listen(port)
+function reply(reply_token, msg) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {/I/DVpOQ/ZP1aBVF1YcRbqjA9dLnY77CcFifMSc+oRd0cQLXkR8wwMIeN4Nn6KsQzyo3dpHTp0Rqh/oofQic74RqBgoUhS53oYr3f+bXcXnygU22LUvfjlhgV5AgpjNBDNbmZ+S5MJNHN2RNJvpJbgdB04t89/1O/w1cDnyilFU=}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: msg
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
